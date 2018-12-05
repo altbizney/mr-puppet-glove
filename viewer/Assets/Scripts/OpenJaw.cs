d75d06c2dd3652a944f7cc1435e2a7ead0a730f0
Jacob Bijani <jake@users.noreply.github.com>
2018-12-04 20:27:16 -0500

    read pose extremes from proxy game objects

--- viewer/Assets/Scripts/OpenJaw.cs
+++ viewer/Assets/Scripts/OpenJaw.cs
@@ -7,7 +7,9 @@ public class OpenJaw : MonoBehaviour
     public float flexMin = 9200.0f;
     public float flexMax = 35000.0f;
 
-    public Transform jointTop;
+    public Transform joint;
+    public Transform initialPose;
+    public Transform extremePose;
 
     [RangeAttribute(0f, 1f)]
     public float speed = 0.035f;
@@ -16,14 +18,15 @@ public class OpenJaw : MonoBehaviour
 
     private float _flex;
     private float flexVelocity = 0f;
+    private float _flex_norm;
 
     void Update()
     {
         _flex = Mathf.SmoothDamp(_flex, Glove.flex, ref flexVelocity, speed);
+        _flex_norm = Mathf.InverseLerp(flexMin, flexMax, _flex);
 
-        jointTop.localPosition = new Vector3(
-            0f, Mathf.Lerp(0f, -moveRange, Mathf.InverseLerp(flexMin, flexMax, _flex)), 0f
-        );
+        joint.localPosition = Vector3.Lerp(initialPose.localPosition, extremePose.localPosition, _flex_norm);
+        joint.localRotation = Quaternion.Lerp(initialPose.localRotation, extremePose.localRotation, _flex_norm);
 
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
