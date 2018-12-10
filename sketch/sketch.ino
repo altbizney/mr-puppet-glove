9f1c38fe3a377e904dec714c4c7f0f6ee6656c6d
Jacob Bijani <jake@users.noreply.github.com>
2018-12-10 15:45:10 -0500

    remove flex value

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -5,14 +5,6 @@
 
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
-const int FLEX_PIN = A0; // Pin connected to flex sensor voltage divider output
-
-// Measure the voltage at 5V and the actual resistance of your
-// 47k resistor, and enter them below:
-const float FLEX_VCC = 4.98; // Measured voltage of Ardunio 5V line
-const float FLEX_R_DIV = 47500.0; // Measured resistance of 3.3k resistor
-
-
 void setup(void)
 {
   Serial.begin(250000); // You can choose any baudrate, just need to also change it in Unity.
@@ -26,9 +18,6 @@ void setup(void)
     while (1);
   }
 
-  // listen to flex pin
-  pinMode(FLEX_PIN, INPUT);
-
   delay(1000);
 
   bno.setExtCrystalUse(true);
@@ -38,10 +27,6 @@ void setup(void)
 
 void loop(void)
 {
-  // Read the ADC, and calculate voltage and resistance from it
-  int flexADC = analogRead(FLEX_PIN);
-  float flexV = flexADC * FLEX_VCC / 1023.0;
-  float flexR = FLEX_R_DIV * (FLEX_VCC / flexV - 1.0);
 
   /*
     imu::Quaternion quat = bno.getQuat();
@@ -63,7 +48,7 @@ void loop(void)
 
 
   Serial.print(",");
-  Serial.print(flexR);
+  Serial.print("0");
   Serial.println("");
 
   /*
