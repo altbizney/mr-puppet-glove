702c809812fd442c186f37ed8b1c38a0a8b087a7
Jacob Bijani <jake@users.noreply.github.com>
2019-03-25 21:09:04 -0400

    fork to simulator

---
+++ simulator/simulator.ino
@@ -0,0 +1,70 @@
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+void setup(void)
+{
+  Serial.begin(250000); // You can choose any baudrate, just need to also change it in Unity.
+  while (!Serial); // wait for Leonardo enumeration, others continue immediately
+
+  /* Initialise the sensor */
+  if (!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.println("DEBUG: Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while (1);
+  }
+
+  delay(1000);
+
+  bno.setExtCrystalUse(true);
+
+  Serial.println("DEBUG: Setup complete");
+}
+
+void loop(void)
+{
+  Serial.print("TS:");
+  Serial.print(millis());
+  
+  imu::Quaternion quat = bno.getQuat();
+  Serial.print(";Q:");
+  Serial.print(quat.x(), 4);
+  Serial.print(",");
+  Serial.print(quat.y(), 4);
+  Serial.print(",");
+  Serial.print(quat.z(), 4);
+  Serial.print(",");
+  Serial.print(quat.w(), 4);
+
+  /*
+    imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+    Serial.print("E,");
+    Serial.print(euler.x());
+    Serial.print(",");
+    Serial.print(euler.y());
+    Serial.print(",");
+    Serial.print(euler.z());
+  */
+
+  Serial.print(";H:");
+  Serial.print(analogRead(A0));
+
+  uint8_t system, gyro, accel, mag = 0;
+  bno.getCalibration(&system, &gyro, &accel, &mag);
+  Serial.print(";C:Sys=");
+  Serial.print(system, DEC);
+  Serial.print(" Gyro=");
+  Serial.print(gyro, DEC);
+  Serial.print(" Accel=");
+  Serial.print(accel, DEC);
+  Serial.print(" Mag=");
+  Serial.print(mag, DEC);
+
+  Serial.println("");
+
+  delay(60);
+}
